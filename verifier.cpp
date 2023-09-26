62f13153b14b5b44d945e841aed876ba58ab5d5b
興怡 <>
2023-09-26 12:14:19 -0700

    follow mbedtls in arduino from dfrobot article

--- verifier.cpp
+++ verifier.cpp
@@ -2,6 +2,16 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <mbedtls/platform.h>
+#include <mbedtls/sha256.h>
+#include <mbedtls/sha512.h>
+#include <mbedtls/oid.h>
+#include <mbedtls/entropy.h>
+#include <mbedtls/error.h>
+#include <mbedtls/md.h>
+#include <mbedtls/bignum.h>
+#include <mbedtls/hmac_drbg.h>
+
 
 // ****TODO**** move to the secrets.h ?
 const char* ssid = "YOURSSID";
@@ -12,8 +22,27 @@ int color = 20; // orange
 AsyncWebServer server(80);
 // landing page delegate
 void onIndexRequest(AsyncWebServerRequest *request) {
-  Serial.println("[placeholder] HTTP GET request of ");
-  request->send(200, "text/plain", "Hello World, its Arduino!");
+  ////Serial.println("[placeholder] HTTP GET request of ");
+  char payload[24] = "Hello SHA 256!";
+  byte shaResult[32];
+  mbedtls_md_context_t ctx;
+  mbedtls_md_type_t md_type = MBEDTLS_MD_SHA256;
+  const size_t payloadLength = strlen(payload);
+  mbedtls_md_init(&ctx);
+  mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(md_type), 0);
+  mbedtls_md_starts(&ctx);
+  mbedtls_md_update(&ctx, (const unsigned char *) payload, payloadLength);
+  mbedtls_md_finish(&ctx, shaResult);
+  mbedtls_md_free(&ctx);
+
+  Serial.print("Hash: ");
+  for(int i=0; i < sizeof(shaResult); i++) {
+    char str[3];
+    sprintf(str, "%02x", (int)shaResult[i]);
+    Serial.print(str);
+  }
+  Serial.println();
+  request->send(200, "text/plain", "..... did we just mbedTLS successfully?");
 }
 // 404 delegate
 void notFound(AsyncWebServerRequest *request) {
